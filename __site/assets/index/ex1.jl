b0c2576218e8a8b4ae6180a1451d1b63117004be
Likan Zhan <zhanlikan@hotmail.com>
2021-03-15 23:44:15 +0800

    rm site

--- __site/assets/index/ex1.jl
+++
@@ -1,6 +0,0 @@
-# This file was generated, do not modify it. # hide
-using LinearAlgebra, Random
-Random.seed!(135)
-a, b = randn(50), randn(50)
-println(dot(a, b))
-println(sum(ai * bi for (ai, bi) ∈ zip(a, b)))
